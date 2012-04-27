b2ff356955756af86cf9b11c33e6c69378740dc2
Detlef Oertel <d.oertel@uib.de>
2012-04-27 14:23:15 +0000

    hwaudit (4.0.4-3)

---
+++ src/setup.py
@@ -0,0 +1,74 @@
+from distutils.core import setup
+import py2exe, sys, os, shutil
+
+# If run without args, build executables, in quiet mode.
+if (len(sys.argv) == 1):
+	sys.argv.append("py2exe")
+	sys.argv.append("-q")
+
+if os.path.exists('dist'):
+	shutil.rmtree('dist')
+
+def tree(src):
+	list = [(root, map(lambda f: os.path.join(root, f), files)) for (root, dirs, files) in os.walk(os.path.normpath(src))]
+	new_list = []
+	for (root, files) in list:
+		if (len(files) > 0) and (root.count('.svn') == 0):
+			new_list.append((root, files))
+	return new_list
+
+class Target:
+	def __init__(self, **kw):
+		self.__dict__.update(kw)
+		self.company_name = "uib GmbH"
+		self.copyright = "uib GmbH"
+		self.version = ""
+		f = open(self.script, 'r')
+		for line in f.readlines():
+			if (line.find("__version__") != -1):
+				self.version = line.split('=', 1)[1].strip()[1:-1]
+				break
+		f.close()
+		if not self.version:
+			print >> sys.stderr, "Failed to find version of script '%s'" % self.script
+
+hwaudit = Target(
+	name = "hwaudit",
+	description = "hardware invent",
+	script = "hwaudit.py",
+	icon_resources = [(1, "hwaudit.ico")]
+)
+
+################################################################
+# COM pulls in a lot of stuff which we don't want or need.
+
+excludes = [	"pywin", "pywin.debugger", "pywin.debugger.dbgcon",
+		"pywin.dialogs", "pywin.dialogs.list",
+		"Tkconstants", "Tkinter", "tcl", "_imagingtk",
+		"PIL._imagingtk", "ImageTk", "PIL.ImageTk", "FixTk"
+]
+
+setup(
+	options = {
+		"py2exe": {
+			"compressed": 1,
+			#"bundle_files": 1,
+			"optimize": 2,
+			"excludes": excludes,
+			"packages": ["OPSI"]
+		}
+	},
+	data_files = [],
+	zipfile = "lib/library.zip",
+	console = [ hwaudit ],
+)
+
+print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+print "!!!   On the target machine always replace exe AND lib   !!!"
+print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+
+
+
+
+
