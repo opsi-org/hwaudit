6ffcd3c17c0b4ec726cd251ef002d36e401d328a
Niko Wenselowski <n.wenselowski@uib.de>
2017-11-27 16:38:59 +0100

    Being more specific what to import from the logging module.

--- src/hwaudit.py
+++ src/hwaudit.py
@@ -14,7 +14,7 @@ from OPSI.Util import *
 from OPSI.Object import *
 from OPSI.Types import *
 from OPSI.System import *
-from OPSI.Logger import *
+from OPSI.Logger import Logger, LOG_ERROR, LOG_DEBUG2
 
 __version__ = "4.0.5.1"
 
