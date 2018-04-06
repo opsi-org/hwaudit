c35e736b921c281ac16443fc534668aec87421ba
Niko Wenselowski <n.wenselowski@uib.de>
2018-04-06 14:16:49 +0200

    Releasing this as 4.1.0.1.

--- src/hwaudit.py
+++ src/hwaudit.py
@@ -16,7 +16,7 @@ from OPSI.Types import (
 	forceUnicode, forceUnicodeList)
 from OPSI.Logger import Logger, LOG_ERROR, LOG_DEBUG2
 
-__version__ = "4.0.7.1"
+__version__ = "4.1.0.1"
 
 logger = Logger()
 
